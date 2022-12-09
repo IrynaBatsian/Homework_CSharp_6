92e9f94cffe4432c08cd1ff0421722caec54f22c
Iryna <batsianiryna@gmail.com>
2022-12-10 00:17:58 +0300

    add task1 пользователь вводит количество элементов и элементы массива, программа считает положительные числа в массиве

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел больше 0 ввел пользователь.
+
+
+void InputArray(int[] array) //заполнение массива
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int ReleaseArray(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    count++;
+  }
+  return count;
+
+}
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[m]; //Создание массива с количеством элементов, которое задал пользователь.
+InputArray(array);
+Console.WriteLine($"Ваш массив: [{string.Join(", ", array)}]");
+Console.WriteLine($"В данном массиве положительных чисел: {ReleaseArray(array)}");
